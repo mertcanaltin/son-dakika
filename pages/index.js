4073d16457be9f4b66a9ece79111cb7fc31d1e86
Mert Can Altın <mertgold60@gmail.com>
2023-02-07 20:41:23 +0300

    delete service & added component key

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import TimeLine from "@/components/timeLine"
-import AlertElement from "@/components/alert"
+import TimeLine from "@/pages/components/timeLine"
+import AlertElement from "@/pages/components/alert"
 
 export default function Home() {
   return (
     <div className="container mx-auto mt-10">
       <AlertElement/>
       <div className="p-20  ">
-        <TimeLine />
+        <TimeLine key={1} />
       </div>
       
     </div>
