3b73f8ed956896c88a2ffdbb7529513fe04517a3
Mert Can Altın <mertgold60@gmail.com>
2023-02-08 17:11:04 +0300

    style component edite

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,9 @@ export default function Home() {
   return (
     <div className="container mx-auto mt-10">
       <AlertElement/>
-      <div className="p-20  ">
+      <div className="p-4 m-4">
         <TimeLine key={1} />
       </div>
-      
     </div>
   )
 }
